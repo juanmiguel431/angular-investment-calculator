647634720f23fe5fac0c297e845921a388091cea
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-22 17:44:32 -0400

    Add user input component with form and styling, integrate into app layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserInputComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-input',
+  imports: [],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {}
